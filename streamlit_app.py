cb7e05556b99f7e79e7708440145522c96c12935
SPSingh21 <88144979+SPSingh21@users.noreply.github.com>
2023-10-16 10:21:02 +0530

    Update streamlit_app.py
    Added another item and smileys.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 and Bueberry Oatmeals')
-streamlit.text('Kale, spinach & rocket smoothie')
-streamlit.text('Hard-Boiled free range eggs')
+streamlit.text('🥣🥑Omega 3 and Bueberry Oatmeals')
+streamlit.text('🥗 Kale, spinach & rocket smoothie')
+streamlit.text('🐔 Hard-Boiled free range eggs')
+streamlit.text('🥑 Avacado Toast')
