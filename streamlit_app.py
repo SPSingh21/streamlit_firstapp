055c2d4efbc8155f3e66747d2fbbbe3ac7a54011
SPSingh21 <88144979+SPSingh21@users.noreply.github.com>
2023-10-16 10:16:36 +0530

    Update streamlit_app.py
    Error Correction

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
-steamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 and Bueberry Oatmeals')
 streamlit.text('Kale, spinach & rocket smoothie')
 streamlit.text('Hard-Boiled free range eggs')
